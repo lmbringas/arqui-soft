c21a7a5f0d622f8f12d89f75c20410a71cd427e3
Lautaro Bringas <lm.bringas98@gmail.com>
2019-11-04 16:52:51 -0300

    -.-

---
+++ src/main/java/ar/edu/ucc/arqsoft/test/model/User.java
@@ -0,0 +1,10 @@
+package ar.edu.ucc.arqsoft.test.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User extends ObjetoGenerico {
+
+}
