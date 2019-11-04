d1c5c50a2fe8c856c2676f038cf9f55f95deb315
Lautaro Bringas <lm.bringas98@gmail.com>
2019-11-04 17:04:25 -0300

    user

--- src/main/java/ar/edu/ucc/arqsoft/test/model/User.java
+++ src/main/java/ar/edu/ucc/arqsoft/test/model/User.java
@@ -1,10 +1,46 @@
 package ar.edu.ucc.arqsoft.test.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "User")
 public class User extends ObjetoGenerico {
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "dni")
+    private String dni;
+
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDni() {
+        return this.dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
 
 }
